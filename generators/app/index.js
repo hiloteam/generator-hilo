3080913f7888e36c500c1bb6e34ae1d5ac84990a
墨水 <wangjin.wj@alibaba-inc.com>
2016-01-27 11:49:52 +0800

    add tests

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,138 +1,135 @@
 'use strict';
 var yeoman = require('yeoman-generator');
-var colors = require('colors');
-var path = require('path');
 var _ = require('lodash');
 var gulpTransformModule = require('gulp-transform-module');
 var gulpFilter = require('gulp-filter');
+require('colors');
 
 module.exports = yeoman.generators.Base.extend({
-    constructor:function(){
-        yeoman.generators.Base.apply(this, arguments);
-        this.props = {};
-    },
+  constructor: function () {
+    yeoman.generators.Base.apply(this, arguments);
+    this.props = {};
+  },
 
-    initializing:function(){
-        this.log('Welcome to the Game world created by Hilo Enginee. You\'re using the fantastic Hilo generator.'.magenta);
-    },
+  initializing: function () {
+    this.log('Welcome to the Game world created by Hilo Enginee. You\'re using the fantastic Hilo generator.'.magenta);
+  },
 
-    prompting: function () {
-        var done = this.async();
-        this.log('Please check your preferene of your game project'.magenta);
+  prompting: function () {
+    var done = this.async();
+    this.log('Please check your preferene of your game project'.magenta);
 
-        var prompts = [{
-              name: 'name',
-              message: 'Your game name',
-              default: 'game'
-            },{
-              name: 'desc',
-              message: 'Description',
-              default: 'a game'
-            },{
-              name: 'authorName',
-              message: 'Author\'s Name',
-              default: this.user.git.name(),
-              store:true
-            },{
-              name: 'authorEmail',
-              message: 'Author\'s Email',
-              default: this.user.git.email(),
-              store:true
-            },{
-              name: 'moduleType',
-              type: 'list',
-              choices: ['amd', 'commonjs', 'kissy', 'cmd'],
-              store: true,
-              message: 'Which kind of module defined do you want to use in your project?',
-              default: 'requirejs'
-            }
-        ];
+    var prompts = [{
+      name: 'name',
+      message: 'Your game name',
+      default: 'game'
+    }, {
+      name: 'desc',
+      message: 'Description',
+      default: 'a game'
+    }, {
+      name: 'authorName',
+      message: 'Author\'s Name',
+      default: this.user.git.name(),
+      store: true
+    }, {
+      name: 'authorEmail',
+      message: 'Author\'s Email',
+      default: this.user.git.email(),
+      store: true
+    }, {
+      name: 'moduleType',
+      type: 'list',
+      choices: ['amd', 'commonjs', 'kissy', 'cmd'],
+      store: true,
+      message: 'Which kind of module defined do you want to use in your project?',
+      default: 'amd'
+    }];
 
-        this.prompt(prompts, function(props){
-            this.props = _.merge(this.props, props);
-            done();
-        }.bind(this));
-    },
+    this.prompt(prompts, function (props) {
+      this.props = _.merge(this.props, props);
+      done();
+    }.bind(this));
+  },
 
-    default:{
-        license:function(){
-            if(!this.props.license){
-                this.composeWith('license', {
-                    options: {
-                        name: this.props.authorName,
-                        email: this.props.authorEmail
-                    }
-                },{
-                    local: require.resolve('generator-license/app')
-                });
-            }
-        },
-        save:function(){
-            // this.config.save();
-        }
-    },
-
-    writing: {
-        copy:function(){
-            this.props.license = this.props.license||false;
-
-            var noTplPath = ['.gitignore', 'src/images'];
-            var tplPath = ['package.json', 'README.md'];
-
-            var that = this;
-
-            var copy = function(templatePath, destinationPath){
-              destinationPath = destinationPath||templatePath;
-              that.fs.copy(that.templatePath(templatePath), that.destinationPath(destinationPath));
-            };
-            var copyTpl = function(templatePath, destinationPath, props){
-              destinationPath = destinationPath||templatePath;
-              props = props||that.props;
-              that.fs.copyTpl(that.templatePath(templatePath), that.destinationPath(destinationPath), props);
-            }
-            noTplPath.forEach(function(path){
-              copy(path);
-            });
-            tplPath.forEach(function(path){
-              copyTpl(path);
-            });
-
-            //index.html
-            copyTpl(this.props.moduleType + '/index.html', that.destinationPath('index.html'));
-            //app js
-            copyTpl('src/js/app/', 'src/js/' + this.props.name + '/');
-            //hilo
-            copy('src/js/hilo/' + that.props.moduleType + '/', 'src/js/hilo/');
-
-            //module
-            switch(that.props.moduleType){
-              case 'amd':
-                copy('amd/requirejs/', 'src/js/requirejs/');
-                break;
-              case 'cmd':
-                copy('cmd/seajs/', 'src/js/seajs/');
-                break;
-              case 'commonjs':
-                copyTpl('commonjs/entry.js', 'src/js/entry.js');
-                copyTpl('commonjs/webpack.config.js', 'webpack.config.js');
-                break;
-            }
-        },
-        transformStream:function(){
-            var moduleFilter = gulpFilter('src/js/' + this.props.name+ '/**/*.js', {restore:true});
-            this.registerTransformStream(moduleFilter);
-            this.registerTransformStream(gulpTransformModule(this.props.moduleType));
-            this.registerTransformStream(moduleFilter.restore);
-        }
-    },
-
-    install: function () {
-        this.installDependencies({
-            bower: false
+  default: {
+    license: function () {
+      if (!this.props.license) {
+        this.composeWith('license', {
+          options: {
+            name: this.props.authorName,
+            email: this.props.authorEmail
+          }
+        }, {
+          local: require.resolve('generator-license/app')
         });
-    },
-
-    end:function(){
-        this.log('goodbye!');
+      }
     }
+  },
+
+  writing: {
+    copy: function () {
+      this.props.license = this.props.license || false;
+
+      var noTplPath = ['.gitignore', 'src/images'];
+      var tplPath = ['package.json', 'README.md'];
+
+      var that = this;
+
+      var copy = function (templatePath, destinationPath) {
+        destinationPath = destinationPath || templatePath;
+        that.fs.copy(that.templatePath(templatePath), that.destinationPath(destinationPath));
+      };
+      var copyTpl = function (templatePath, destinationPath, props) {
+        destinationPath = destinationPath || templatePath;
+        props = props || that.props;
+        that.fs.copyTpl(that.templatePath(templatePath), that.destinationPath(destinationPath), props);
+      };
+      noTplPath.forEach(function (path) {
+        copy(path);
+      });
+      tplPath.forEach(function (path) {
+        copyTpl(path);
+      });
+
+      //index.html
+      copyTpl(this.props.moduleType + '/index.html', that.destinationPath('index.html'));
+      //app js
+      copyTpl('src/js/app/', 'src/js/' + this.props.name + '/');
+      //hilo
+      copy('src/js/hilo/' + that.props.moduleType + '/', 'src/js/hilo/');
+
+      //module
+      switch (that.props.moduleType) {
+        case 'amd':
+          copy('amd/requirejs/', 'src/js/requirejs/');
+          break;
+        case 'cmd':
+          copy('cmd/seajs/', 'src/js/seajs/');
+          break;
+        case 'commonjs':
+          copyTpl('commonjs/entry.js', 'src/js/entry.js');
+          copyTpl('commonjs/webpack.config.js', 'webpack.config.js');
+          break;
+      }
+    },
+    transformStream: function () {
+      var moduleFilter = gulpFilter('src/js/' + this.props.name + '/**/*.js', {
+        restore: true
+      });
+      this.registerTransformStream(moduleFilter);
+      this.registerTransformStream(gulpTransformModule(this.props.moduleType));
+      this.registerTransformStream(moduleFilter.restore);
+    }
+  },
+
+  install: function () {
+    this.installDependencies({
+      bower: false
+    });
+  },
+
+  end: function () {
+    this.log('goodbye!');
+  }
 });
--- test/app.js
+++ test/app.js
@@ -3,17 +3,57 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-generator').test;
 
-describe('generator-hilo:app', function () {
-  before(function (done) {
-    helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({someOption: true})
-      .withPrompts({someAnswer: true})
-      .on('end', done);
-  });
+var moduleTypes = {
+  amd:[
+    'src/js/requirejs'
+  ],
+  commonjs:[
+    'webpack.config.js',
+    'src/js/entry.js'
+  ],
+  cmd:[
+    'src/js/seajs'
+  ],
+  kissy:[
 
-  it('creates files', function () {
-    assert.file([
-      'dummyfile.txt'
-    ]);
-  });
-});
+  ]
+};
+
+var prompts = {
+  name:'testGame',
+  desc:'a test game',
+  authorName:'06wj',
+  authorEmail:'06wj@163.com',
+  moduleType:'amd',
+  license:'MIT'
+};
+
+var commonFiles = [
+  'index.html',
+  'package.json',
+  'LICENSE',
+  '.gitignore',
+  'src/images',
+  'src/js/testGame'
+];
+
+for(var moduleType in moduleTypes){
+  (function(moduleType){
+    var files = moduleTypes[moduleType];
+    describe('generator-hilo:' + moduleType, function () {
+      before(function (done) {
+        prompts.moduleType = moduleType;
+        helpers.run(path.join(__dirname, '../generators/app'))
+          .withOptions({})
+          .withPrompts(prompts)
+          .on('end', done);
+      });
+
+      it('creates files', function () {
+        assert.file(commonFiles.concat(files));
+      });
+    });
+  })(moduleType);
+}
+
+
